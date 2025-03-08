0c9f0104cd0aa47fc8496f5f79bc5a894d1e978a
jjfire04 <jjfirewin@gmail.com>
2025-03-08 20:30:06 +0000

    commit

---
+++ super_trunfo.c/super_trunfo.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main (){
+    
+    char estado[15];
+    char Codigo[5];
+    char cidade[20];
+    int Populacao;
+    float Area;
+    float Pib;
+    int Turismo;
+
+    printf("digite o estado: \n");
+    scanf("%s", &estado);
+    
+    printf("digite o codigo: \n");
+    scanf("%s", &Codigo);
+    
+    printf("digite o nome da cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("digite a quantidade de populacao: \n");
+    scanf("%d", &Populacao);
+
+    printf("digite o tamanho da area km: \n");
+    scanf("%f", &Area);
+
+    printf("digite o Pib: \n");
+    scanf("%f", &Pib);
+
+    printf("digite quantos pontos turisticos tem na cidade: \n");
+    scanf("%d\n", &Turismo);
+
+    printf("carta 1\n");
+    printf("O estado é: %s\n", estado);
+    printf("O codigo da carta é: %s\n", Codigo);
+    printf("O nome da cidade é: %s\n", cidade);
+    printf("A quantidade de população é: %d\n", Populacao);
+    printf("O tamanha da area é: %f\n", Area);
+    printf("O PIB é: %f\n", Pib);
+    printf("pontos turisticos é: %d\n", Turismo);
+    
+    return 0;
+
+}
